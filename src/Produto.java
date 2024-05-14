25179ec460d2b562a95871ac1a7c569eb62e125b
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-05-14 08:28:14 -0300

    .

--- src/Produto.java
+++ src/Produto.java
@@ -1,28 +1,33 @@
-public class Produto extends Tipo {
-    private String nomeProd;
+public class Produto{
+    private String nome;
+    private Tipo tipo;
     private String marca;
     private float preco;
     public Produto(){
         super();
-        this.nomeProd = "xxxx";
+        this.nome = "xxxx";
         this.marca = "xxxxx";
         this.preco = 00.0f;
     }
-    public Produto(String nome, String nomeProd, String marca, float preco){
-        super(nome);
-        this.nomeProd = nomeProd;
+    public Produto(Tipo tipo, String nome, String marca, float preco){
+        this.tipo = tipo;
+        this.nome = nome;
         this.marca = marca;
         this.preco = preco;
     }
 
-    public String getNomeProd() {
-        return nomeProd;
+    public String getNome() {
+        return nome;
     }
     public String getMarca() {
         return marca;
     }
     public float getPreco() {
         return preco;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
     }
     public void setPreco(float preco) {
         this.preco = preco;
--- src/Tipo.java
+++ src/Tipo.java
@@ -1,4 +1,4 @@
-public abstract class Tipo {
+public class Tipo {
     protected String nome;
 
     public Tipo(){
