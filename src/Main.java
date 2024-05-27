5138ae311a149b2a749a9f8b459b37727bda6685
lena <helenacarvalhoferreira05@gmail.com>
2024-05-27 15:50:35 -0300

    estrutura menu cliente

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Mercado mercado = new Mercado();
-        MenuColaborador menu = new MenuColaborador(mercado);
-        if(menu.login()){
-            menu.mostrarMenu();
-        }else{
+        Scanner scanner = new Scanner(System.in);
+
+        boolean mostrando = true;
+        while (mostrando) {
+            System.out.println("\n**** SELECIONE O MENU DESEJADO ****");
+            System.out.println("1. Menu Cliente");
+            System.out.println("2. Menu Colaborador");
+            System.out.println("0. Sair");
+            System.out.println("Digite a opção: ");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if (opcao == 1) {
+                Cliente cliente = new Cliente();
+                MenuCliente menu = new MenuCliente(cliente);
+                menu.mostrarMenu();
+            } else if (opcao == 2) {
+                Mercado mercado = new Mercado();
+                MenuColaborador menu = new MenuColaborador(mercado);
+                if (menu.login()) {
+                    menu.mostrarMenu();
+                } else {}
+                System.out.println("\nEncerrando...");
+            } else if (opcao == 0) {
+                mostrando = false;
+                System.out.println("Saindo...");
+                break;
+            } else {
+                System.out.println("Opção inválida");
+            }
+
         }
-        System.out.println("\nEncerrando...");
+
     }
 }
---
+++ src/MenuCliente.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class MenuCliente {
+
+    private Scanner scanner;
+    private Cliente cliente;
+
+    public MenuCliente(Cliente cliente) {
+        this.scanner = new Scanner(System.in);
+        this.cliente = cliente;
+    }
+
+    public void mostrarMenu() {
+        boolean mostrando = true;
+        while (mostrando) {
+            System.out.println("\n--- Menu Cliente ---");
+            System.out.println("1. Criar conta");
+            System.out.println("2. Ver produtos");
+            System.out.println("3. Ver carrinho");
+            System.out.println("4. Ver dados");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Criando conta");
+                    break;
+                case 2:
+                    System.out.println("Vendo produtos");
+                    break;
+                case 3:
+                    System.out.println("Vendo carrinho");
+                    break;
+                case 4:
+                    System.out.println("Vendo dados...");
+                    break;
+                case 0:
+                    mostrando = false;
+                    System.out.println("Saindo");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+
+}
