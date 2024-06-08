50fdfb926a81335aa0709aec7a09a0c55807ee90
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-06-08 16:47:03 -0300

    cadastrar funcionario

--- src/Mercado.java
+++ src/Mercado.java
@@ -311,4 +311,8 @@ public class Mercado implements Serializable {
         catch (FileNotFoundException e){}
         catch (IOException e){}
     }
+
+    public void adicionarColaborador(Colaborador novoColaborador) {
+        colaboradores.add(new Colaborador("Natalia", "nati@gmail.com" ,"Admin",123,"Administrador"));
+    }
 }
